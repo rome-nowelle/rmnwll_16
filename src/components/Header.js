e96ed21d0169e19cac2892aceae8a57b1dd066bc
Rome Nowelle R. Sanares <romenowellesanares@gmail.com>
2026-01-25 10:29:06 +0800

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,50 @@
 import React from "react";
-import "./styles/Header.css";
+import "./styles/header.css";
 import Closed from "@mui/icons-material/Close";
 import MenuBookOutlined from "@mui/icons-material/MenuBookOutlined";
 import { Link } from "react-router-dom";
+import logo from '../assets/logo.png';
 
 const Header = () => {
-    return (
-        <div className="Header">
-            <h1>Rmnwll_</h1>
 
-            <nav>
+    const [active, setActive] = React.useState(false);
+
+        function showMenu() {
+        setActive(!active);
+    }
+        
+    return (
+        <div className="header">
+            <div className="logo">
+                <img src={logo} alt="Rmnwll_" />
+                <h1>Rmnwll_</h1>
+            </div>
+
+            <nav className="navbar">
                 <ul>
-                    <div className="Closed">
-                        <Closed className="close" />
+                    <div className="closed">
+                        <Closed className="close" onClick={showMenu} />
                     </div>
                     <li>
                         <Link to="/">About</Link>
+                    </li>
+                    <li>
                         <Link to="/">Resume</Link>
+                    </li>
+                    <li>
                         <Link to="/">Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Skills</Link>
+                    </li>
+                    <li>
                         <Link to="/">Contact</Link>
                     </li>
                 </ul>
             </nav>
 
             <div className="menu">
-                <MenuBookOutlined className="menu" />
+                <MenuBookOutlined className="menu-icon" onClick={showMenu} />
             </div>
         </div>
     );
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/main.css";
+
+
+const Main = () => {
+    return (
+        <div className="main">
+            <h2>Welcome to the Main Component</h2>
+        </div>  
+    )
+}
+
+export default Main;
